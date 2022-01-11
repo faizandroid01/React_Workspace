17fe274472468a71bb1777a62b66cff0a3e06b1a
FAIZ <faizrocx9211@gmail.com>
2022-01-12 01:23:40 +0530

    # Extracting Component, # Wrapper Component, # React Module Import , # traditional JSX , # Organizing the component.

--- ReactProjects/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ ReactProjects/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,18 @@
-import ExpenseDate from "./ExpenseDate";
+import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css";
+import Card from "../UI/Card.js";
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date (2022, 1 , 8);
-  // const expenseTitle = 'A new Laptop' ;
-  // const expenseAmount = 247.69;
-
-  //extracting respective values from a date object
-
   
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       
       <div className="expense-item__description">
         <ExpenseDate date={props.date}/>
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
---
+++ ReactProjects/react-complete-guide/src/components/UI/Card.js
@@ -0,0 +1,10 @@
+import "./Card.css";
+
+function Card(props) {
+  // ways to mix css classes
+  const classes = "card " + props.className;
+  // {props.children} renders the childen component fom the incoming prop
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
