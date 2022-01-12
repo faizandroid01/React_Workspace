3856884e28afd94cdcb8fda7fd044b7890adbc9d
FAIZ <faizrocx9211@gmail.com>
2022-01-12 17:35:33 +0530

    React Hooks - useState usage.

--- ReactProjects/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ ReactProjects/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,26 @@
+import React, {useState} from 'react';
 import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css";
 import Card from "../UI/Card.js";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
   
+  const clickHandler = () => {
+    // title = 'Updated'  It wont be set like this anymore , insteasd through
+    // a dedicated function through useState
+    setTitle('Updated');
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       
       <div className="expense-item__description">
         <ExpenseDate date={props.date}/>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title </button>
       </div>
     </Card>
   );
--- ReactProjects/react-complete-guide/src/components/Expenses/Expenses.js
+++ ReactProjects/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import ExpenseItem from "./ExpenseItem.js";
 import "./Expenses.css";
 import Card from "../UI/Card.js"
