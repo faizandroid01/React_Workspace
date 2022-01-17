58af709e237e410c6672b710338ef8212b3d5947
FAIZ <faizrocx9211@gmail.com>
2022-01-17 08:40:38 +0530

    Added Expense Filter with the practise usage of data transefer from Child to parent  && 2way binding on useState Var

--- ReactProjects/react-complete-guide/src/components/Expenses/Expenses.js
+++ ReactProjects/react-complete-guide/src/components/Expenses/Expenses.js
@@ -3,11 +3,23 @@ import React from 'react';
 import ExpenseItem from "./ExpenseItem.js";
 import "./Expenses.css";
 import Card from "../UI/Card.js"
+import ExpensesFilter from './ExpensesFilter.js';
+import { useState } from 'react/cjs/react.development';
 
 
 function Expenses(props) {
+
+    const [selectedYear , setSelectedYear] = useState('2020');
+
+  const filterChangeHandler = (changedYear) => {
+    //console.log(chnagedYear);
+    setSelectedYear(changedYear);
+  };
+
   return (
+    <div>
     <Card className="expenses">
+      <ExpensesFilter selected={selectedYear} onFilterChange={filterChangeHandler}/>
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -29,6 +41,7 @@ function Expenses(props) {
         date={props.expenses[3].date}
       />
     </Card>
+    </div>
   );
 }
 
